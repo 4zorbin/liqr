0547082d14061fc4636097dcf0d41093aabada95
4zorbin <adwell83@gmail.com>
2021-07-11 17:12:12 +0300

    fix not done

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+let codeEl = document.getElementById('text')
 let scanner = new Instascan.Scanner({ video: document.getElementById('preview')});
            Instascan.Camera.getCameras().then(function(cameras){
                if(cameras.length > 0 ){
@@ -15,3 +17,21 @@ let scanner = new Instascan.Scanner({ video: document.getElementById('preview')}
             console.log(codeEl);
 
            });
+
+
+        function saveEl() {
+            let testusEl = document.getElementById("testus")
+
+            // console.log(codeEl);
+                // console.log('checkClick');
+
+            }
+        function addEl() {
+                console.log('addClick');
+            }
+        function dellEl() {
+                console.log('dellClick');
+            }
+            function myFunction() {
+                document.getElementById("myForm").submit();
+              }
